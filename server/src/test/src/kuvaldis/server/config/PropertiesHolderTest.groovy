25793f380434499711165efd852337e2a5187945
Nikolay Fadin <kuvaldis1988@gmail.com>
2014-07-07 17:02:45 +0400

    spock added some tests

---
+++ server/src/test/src/kuvaldis/server/config/PropertiesHolderTest.groovy
@@ -0,0 +1,12 @@
+package kuvaldis.server.config
+
+import spock.lang.Specification
+
+/**
+ * User: NFadin
+ * Date: 07.07.2014
+ * Time: 16:56
+ */
+class PropertiesHolderTest extends Specification {
+    def "should load properties from config object"
+}
