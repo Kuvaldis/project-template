72de1e6aa14ef9df5027a38d7918857aba842bca
Nikolay Fadin <kuvaldis1988@gmail.com>
2014-07-09 12:04:32 +0400

    AppUserRepository change to JpaRepository

--- model/src/main/groovy/kuvaldis/model/data/repository/AppUserRepository.groovy
+++ model/src/main/groovy/kuvaldis/model/data/repository/AppUserRepository.groovy
@@ -1,13 +1,13 @@
 package kuvaldis.model.data.repository
 
 import kuvaldis.model.data.domain.AppUser
-import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
 /**
  * User: NFadin
  * Date: 08.07.2014
  * Time: 16:05
  */
-interface AppUserRepository extends PagingAndSortingRepository<AppUser, Long> {
+interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username)
 }
